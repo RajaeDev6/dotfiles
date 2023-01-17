fded9ff31d39deb10461122a4924bdde2badaad1
rajae <gkid2549@gmail.com>
2023-01-17 15:53:28 -0500

    added packer install

--- install.sh
+++ install.sh
@@ -19,8 +19,6 @@ elif [[ $(uname -s) == "Linux" ]]
 then
     sudo apt install neovim
 fi
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 }
 
 function installStow(){
