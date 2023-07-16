31e1e5a7f35d9d5c48368638ab3d25f0be31065d
RajaeDev6 <rajapple876@gmail.com>
2023-07-15 19:37:10 -0500

    add wallpapers, polybar.picom and i3 config

--- nvim/lua/colorscheme-config.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd[[colorscheme badwolf]]
-vim.cmd[[highlight Cursorline gui=underline ctermbg=NONE guibg=NONE]]
-vim.cmd([[hi Normal guibg=NONE cterm=NONE]])
--- nvim/lua/prettier-config.lua
+++
@@ -1,2 +0,0 @@
-local prettier = require("prettier.nvim")
-vim.api.nvim_command("autocmd BufWritePre *.html,*.js,*.css :call prettier#format_file()")
